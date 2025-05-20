173b7c0582b26ee8a691e846e13ecb0fbc966029
Zakky Muhammad Fajar <zakkymf@gmail.com>
2025-05-20 21:50:13 +0700

    fix: login page

--- src/pages/login/views/Login.tsx
+++ src/pages/login/views/Login.tsx
@@ -4,17 +4,18 @@ import { useLoginController } from "../controllers/useLoginController";
 function Login() {
   const { email, password, handleLogin, setEmail, setPassword } =
     useLoginController();
+
   return (
     <div
       className="d-flex flex-column justify-content-center align-items-center w-100"
-      style={{ height: "100vh" }}
+      style={{ minHeight: "100vh", padding: "1rem" }}
     >
-      <Card className="w-25">
+      <Card style={{ width: "100%", maxWidth: "400px" }}>
         <Card.Body>
           <Card.Title>Login</Card.Title>
 
           <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="loginEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
@@ -24,7 +25,7 @@ function Login() {
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="loginPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type="password"
